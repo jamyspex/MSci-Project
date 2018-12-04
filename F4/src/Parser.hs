bba30e3fd18ef3bdf516b0d68330b194cdfc95ff
james <jameslmacdonald@gmail.com>
2018-12-04 20:14:08 +0000

    work on adding arg maps to subroutine tables

--- F4/src/Parser.hs
+++ F4/src/Parser.hs
@@ -6,8 +6,10 @@ module Parser (Parser.parseFile) where
 import           Language.Fortran
 import           LanguageFortranTools as LFT
 
-parseFile :: [String] -> [String] -> Bool -> String -> IO (Program Anno)
+
+--                                                           AST           Lines    Filename
+parseFile :: [String] -> [String] -> Bool -> String -> IO ((Program Anno, [String], String))
 parseFile cppDArgs cppXArgs fixedForm filename = do
     parseOutput <- LFT.parseFile cppDArgs cppXArgs fixedForm filename
-    let (parsedProgram, _, _) = parseOutput
-    return (fst parsedProgram)
+    let ((parsedProgram, lines), _, _) = parseOutput
+    return (parsedProgram, lines, filename)
