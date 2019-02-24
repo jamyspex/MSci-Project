79e1643eb003962f0c0d1d728265af6325d50368
James Macdonald <jameslmacdonald@gmail.com>
2019-02-24 21:12:05 +0000

    move to desktop

--- F4/src/AddMemoryAccessKernels.hs
+++ F4/src/AddMemoryAccessKernels.hs
@@ -1,18 +1,20 @@
 module AddMemoryAccessKernels where
 
+import           Pipeline
+import           Utils
 
---  The way we're going to work this is take the last instance of an output stream to the where ever 
---  it is next required. If an output stream does not exist prior to that point in the pipeline 
---  generate a memory reader. 
+--  The way we're going to work this is take the last instance of an output stream to the where ever
+--  it is next required. If an output stream does not exist prior to that point in the pipeline
+--  generate a memory reader.
 --
 -- TODO
--- For now I'm going to set the size of the smartcaches to the size of the largest one in the 
+-- For now I'm going to set the size of the smartcaches to the size of the largest one in the
 -- pipeline to prevent deadlocks in situations like shown below. I say for now but I don't think
--- there is anyway to actually get round this issue. The best we could do is to minimise the 
--- number of smart caches that have their size increased by analysing the dependency graph. 
+-- there is anyway to actually get round this issue. The best we could do is to minimise the
+-- number of smart caches that have their size increased by analysing the dependency graph.
 -- Hmm actually I'm just going to leave it for now and then once I've wired all the pipes up decide
--- how much work it would be to implement the dependency analysis as can easily implement the "scale 
--- 'em all up" fall back at that point as well. 
+-- how much work it would be to implement the dependency analysis as can easily implement the "scale
+-- 'em all up" fall back at that point as well.
 --
 --                --------------
 --                |  Kernel 1  |
@@ -20,27 +22,34 @@ module AddMemoryAccessKernels where
 --          stream 1 |       |  stream 2
 --                   |       ---------|
 --                   V                |
---              -------------------   |    This would deadlock because in order for Kernel 
+--              -------------------   |    This would deadlock because in order for Kernel
 -- size = 1005  |  Smart Cache 1  |   |    2 to start processing 1005 items from stream 1 need to be
---              -------------------   |    written to Smart Cache 1. However that is impossible 
---                   |                |    because Smart Cache 2 is only able to buffer 503 
+--              -------------------   |    written to Smart Cache 1. However that is impossible
+--                   |                |    because Smart Cache 2 is only able to buffer 503
 --                   |                |    values of stream 2. Thinking about it this is exactly
 --                   V                |    the same issue that means cache lines within a smart
---                --------------      |    cache have to all be the same size as the largest one. 
+--                --------------      |    cache have to all be the same size as the largest one.
 --                |  Kernel 2  |      |
 --                --------------      |
 --                   |                |
 --                   |       |---------
---                   V       V        
---              -------------------   
---  size = 503  |  Smart Cache 2  |   
---              -------------------   
---                   |       |        
+--                   V       V
+--              -------------------
+--  size = 503  |  Smart Cache 2  |
+--              -------------------
+--                   |       |
 --                   |       |
 --                   V       V
 --                --------------
 --                |  Kernel 3  |
 --                --------------
-
-
-
+-- 
+--
+--
+-- Add memory reader kernels
+addMemoryReaders ::
+     [(Kernel, Pipeline SharedPipelineData)]
+  -> IO [(Kernel, Pipeline SharedPipelineData, [Pipeline SharedPipelineData])]
+addMemoryReaders kernelsAndSmartCaches =
+  where
+    
--- F4/src/F4.hs
+++ F4/src/F4.hs
@@ -2,39 +2,28 @@
 
 module F4 where
 
-import           Parser                         ( parseProgramData )
-import           Utils                          ( SubRec(..)
-                                                , debug_displaySubRoutineTable
-                                                )
+import           Parser                  (parseProgramData)
+import           Utils                   (SubRec (..),
+                                          debug_displaySubRoutineTable)
 
 import           AddKernelLoopGuards
 import           AddSmartCaches
-import           CommandLineProcessor           ( F4Opts(..)
-                                                , f4CmdParser
-                                                )
+import           CommandLineProcessor    (F4Opts (..), f4CmdParser)
 import           ConstantFolding
-import           Data.Generics                  ( everything
-                                                , everywhere
-                                                , everywhereM
-                                                , gmapQ
-                                                , gmapT
-                                                , mkM
-                                                , mkQ
-                                                , mkT
-                                                )
-import qualified Data.Map                      as DMap
+import           Data.Generics           (everything, everywhere, everywhereM,
+                                          gmapQ, gmapT, mkM, mkQ, mkT)
+import qualified Data.Map                as DMap
 import           Debug.Trace
 import           KernelExtraction
 import           Language.Fortran
 import           Language.Fortran.Pretty
-import qualified LanguageFortranTools          as LFT
+import qualified LanguageFortranTools    as LFT
 import           MergeSubroutines
 import           MiniPP
 import           Options.Applicative
 import           SanityChecks
 import           StencilDetection
 import           Transformer
-
 
 processArgs :: IO ()
 processArgs = do
@@ -45,114 +34,68 @@ processArgs = do
 
 compilerMain :: F4Opts -> IO ()
 compilerMain args = do
+  print "Executing compiler main..."
+  -- Parse the Fortran files specified at the command line
   subroutineTable <- parseProgramData args
-
-  -- traceIO $ show (DMap.keys subroutineTable)
-
+  -- seperate out the parsed files to be offloaded to the FPGA
   let notForOffloadSubTable = DMap.filter (not . parallelise) subroutineTable
-  let forOffloadSubTable    = DMap.filter parallelise subroutineTable
-  let subroutineNames       = DMap.keys forOffloadSubTable
-
+  let forOffloadSubTable = DMap.filter parallelise subroutineTable
+  let subroutineNames = DMap.keys forOffloadSubTable
   putStrLn (rule '+' ++ " Subroutines not for offload " ++ rule '+')
-
   debug_displaySubRoutineTable notForOffloadSubTable False
-
-  -- traceIO $ show (DMap.keys notForOffloadSubTable)
-
   putStrLn (rule '+' ++ " Subroutines for offload " ++ rule '+')
-
   debug_displaySubRoutineTable forOffloadSubTable False
-
-  -- traceIO $ show (DMap.keys forOffloadSubTable)
-
   putStrLn (rule '+' ++ " Subroutines for offload merged " ++ rule '+')
-
+  -- Merge the subroutines to be offloaded into one
   let subroutineTableWithOffloadSubsMerged =
         mergeSubsToBeParallelised subroutineTable
-
-  -- traceIO $ show (DMap.keys subroutineTableWithOffloadSubsMerged)
-
   debug_displaySubRoutineTable subroutineTableWithOffloadSubsMerged False
-
   let mergedForOffload =
         DMap.filter parallelise subroutineTableWithOffloadSubsMerged
   let mergedOffloadName = head $ DMap.keys mergedForOffload
-
   putStrLn (rule '+' ++ " Map + Fold Detection " ++ rule '+')
-
+  -- Map and fold detection from Gavin's compiler
   -- < STEP 4 : Parallelise the loops >
   -- WV: this is the equivalent of calling a statefull pass on every subroutine.
-  let (parallelisedSubroutines, parAnnotations) = foldl
-        (paralleliseProgUnit_foldl (ioSubs args)
-                                   subroutineTableWithOffloadSubsMerged
-        )
-        (DMap.empty, [])
-        [mergedOffloadName]
-
+  let (parallelisedSubroutines, parAnnotations) =
+        foldl
+          (paralleliseProgUnit_foldl
+             (ioSubs args)
+             subroutineTableWithOffloadSubsMerged)
+          (DMap.empty, [])
+          [mergedOffloadName]
   debug_displaySubRoutineTable parallelisedSubroutines False
-
-  let srtWithParallelisedSubroutines = DMap.union
-        parallelisedSubroutines
-        subroutineTableWithOffloadSubsMerged
-
+  let srtWithParallelisedSubroutines =
+        DMap.union parallelisedSubroutines subroutineTableWithOffloadSubsMerged
   -- mapM_ (\subRecord -> putStrLn ("\n" ++ hl ++ (fst subRecord) ++ hl ++ (miniPPProgUnit (subAst (snd subRecord))) ++ hl))
   --     (DMap.toList parallelisedSubroutines)
-
   putStrLn (rule '+' ++ " Stencil Detection " ++ rule '+')
-
   let srtAfterStenDetect =
         detectStencilsInSubsToBeParallelise srtWithParallelisedSubroutines
-
   debug_displaySubRoutineTable srtAfterStenDetect False
-
   -- < STEP 5 : Try to fuse the parallelised loops as much as possible (on a per-subroutine basis) >
-  let (combinedKernelSubroutines, combAnnotations) = foldl
-        (combineKernelProgUnit_foldl (loopFusionBound args))
-        (srtAfterStenDetect, [])
-        [mergedOffloadName]
-
+  let (combinedKernelSubroutines, combAnnotations) =
+        foldl
+          (combineKernelProgUnit_foldl (loopFusionBound args))
+          (srtAfterStenDetect, [])
+          [mergedOffloadName]
   putStrLn (rule '+' ++ " Combined " ++ rule '+')
-
   let srtAfterKernelCombination =
         DMap.union combinedKernelSubroutines srtAfterStenDetect
-
   debug_displaySubRoutineTable srtAfterKernelCombination False
-
   let combinedOffloadSub = srtAfterKernelCombination DMap.! mergedOffloadName
-
   putStrLn (rule '+' ++ " With Loop Guards " ++ rule '+')
-
   let withGuards = addLoopGuards combinedOffloadSub
-
   let srtWithGuards =
         DMap.insert mergedOffloadName withGuards srtAfterKernelCombination
-
   debug_displaySubRoutineTable srtWithGuards False
-
   putStrLn (rule '+' ++ " Kernels " ++ rule '+')
-
   let guardedMerged = srtWithGuards DMap.! mergedOffloadName
-
   kernels <- getKernels guardedMerged
-
   putStrLn (rule '+' ++ " With Smart Caches " ++ rule '+')
-
-  -- this is a [(Kernel, SmartCache)] representing kernels and their
-  -- preceding smart cache
+  -- this is a [(Kernel, Maybe SmartCache)] representing kernels and their
+  -- preceding smart cache if one is required
   smartCacheKernelPairs <- insertSmartCaches kernels
-
-  -- let kernelsAndSmartCaches = insertSmartCaches kernels
-  -- mapM_ (\subRecord -> putStrLn ("\n" ++ hl ++ (fst subRecord) ++ hl ++ (miniPPProgUnit (subAst (snd subRecord))) ++ hl))
-  --     (DMap.toList combinedKernelSubroutines)
-
-  -- JM: This is simply so status information can be printed.
-  -- < STEP 6a : create annotation listings >
-  -- let annotationListings = map (combineAnnotationListings_map parAnnotations) combAnnotations
-
-  -- --    < STEP 7a : >
-  -- let argTranslations = extractSubroutineArgumentTranslationMaps combinedKernelSubroutines parsedMain
-  --         -- WV: TODO: put these into SubRec.subCalledSubs.ArgMap or at least in SubRec.subCalledSubsArgMaps
-
   return ()
 
 validateInputFiles :: Program LFT.Anno -> IO ()
@@ -161,11 +104,10 @@ validateInputFiles fileAst = do
   mapM_ printErrorOrContinue results
 
 banner =
-  rule '='
-    ++ "F4: Finite-element Fortran for FPGAs\n"
-    ++ "This compiler allows Fortran finite element codes to be compiled\n"
-    ++ "for execution on FPGA devices via OpenCL"
-    ++ rule '='
+  rule '=' ++
+  "F4: Finite-element Fortran for FPGAs\n" ++
+  "This compiler allows Fortran finite element codes to be compiled\n" ++
+  "for execution on FPGA devices via OpenCL" ++ rule '='
 
 hl = rule '-'
 
