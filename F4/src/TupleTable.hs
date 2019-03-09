386e6a88015c0bfee87061314543cedec9fc1b6d
james <jameslmacdonald@gmail.com>
2019-03-09 20:25:17 +0000

    delete some rubbish out folder

--- F4/src/TupleTable.hs
+++ F4/src/TupleTable.hs
@@ -1,12 +1,19 @@
-module TupleTable                     (TupleTable (..), getMostTuple, getLeastTuple, insertIntoTupleTable, lookupTupleTable, collapseIterTable,
-                                    tupleTableElementGreaterThan, tupleTableNotEmpty)
-
+module TupleTable
+  ( TupleTable(..)
+  , getMostTuple
+  , getLeastTuple
+  , insertIntoTupleTable
+  , lookupTupleTable
+  , collapseIterTable
+  , tupleTableElementGreaterThan
+  , tupleTableNotEmpty
+  )
 where
 
-import qualified Data.Map             as DMap
+import qualified Data.Map                      as DMap
 import           Data.Maybe
 
-import           LanguageFortranTools (listConcatUnique)
+import           LanguageFortranTools           ( listConcatUnique )
 
 --    The TupleTable data structure is used to represent a tree like structure of tuples. it is a trie (https://en.wikipedia.org/wiki/Trie) that has all
 --    its leaf nodes at the same depth. It is used by the compiler to represent combinations of loop iterator values without incuring an explosion of
@@ -19,54 +26,73 @@ data TupleTable = LoopIterRecord (DMap.Map Int TupleTable)
                     deriving (Show, Eq)
 
 tupleTableElementGreaterThan :: [Int] -> [Int] -> Bool
-tupleTableElementGreaterThan [] []             =    False
-tupleTableElementGreaterThan (x:[]) (y:[])    =    x > y
-tupleTableElementGreaterThan [] y             =    False
-tupleTableElementGreaterThan x []             =    True
-tupleTableElementGreaterThan (x:xs) (y:ys)     |    x > y = True
-                                            |    x == y = tupleTableElementGreaterThan xs ys
-                                            |    x < y = False
+tupleTableElementGreaterThan []       []       = False
+tupleTableElementGreaterThan (x : []) (y : []) = x > y
+tupleTableElementGreaterThan []       y        = False
+tupleTableElementGreaterThan x        []       = True
+tupleTableElementGreaterThan (x : xs) (y : ys)
+  | x > y  = True
+  | x == y = tupleTableElementGreaterThan xs ys
+  | x < y  = False
 
 getMostTuple :: TupleTable -> [Int]
-getMostTuple (LoopIterRecord table) = [leastValue] ++ (getMostTuple (DMap.findWithDefault Empty leastValue table))
-            where
-                allowedValues = DMap.keys table
-                leastValue = foldl1 (max) allowedValues
+getMostTuple (LoopIterRecord table) =
+  [leastValue] ++ (getMostTuple (DMap.findWithDefault Empty leastValue table))
+ where
+  allowedValues = DMap.keys table
+  leastValue    = foldl1 (max) allowedValues
 getMostTuple Empty = []
 
 getLeastTuple :: TupleTable -> [Int]
-getLeastTuple (LoopIterRecord table) = [leastValue] ++ (getLeastTuple (DMap.findWithDefault Empty leastValue table))
-            where
-                allowedValues = DMap.keys table
-                leastValue = foldl1 (min) allowedValues
+getLeastTuple (LoopIterRecord table) =
+  [leastValue] ++ (getLeastTuple (DMap.findWithDefault Empty leastValue table))
+ where
+  allowedValues = DMap.keys table
+  leastValue    = foldl1 (min) allowedValues
 getLeastTuple Empty = []
 
 insertIntoTupleTable :: [Int] -> TupleTable -> TupleTable
-insertIntoTupleTable [] tupleTable = tupleTable
-insertIntoTupleTable (indices) Empty = createTupleTable indices
-insertIntoTupleTable (indices) (LoopIterRecord table) = case DMap.lookup (head indices) table of
-                                                            Just subTable -> LoopIterRecord (DMap.insert (head indices) (insertIntoTupleTable (tail indices) subTable) table)
-                                                            Nothing -> LoopIterRecord (DMap.insert (head indices) (createTupleTable (tail indices)) table)
+insertIntoTupleTable []        tupleTable = tupleTable
+insertIntoTupleTable (indices) Empty      = createTupleTable indices
+insertIntoTupleTable (indices) (LoopIterRecord table) =
+  case DMap.lookup (head indices) table of
+    Just subTable -> LoopIterRecord
+      (DMap.insert (head indices)
+                   (insertIntoTupleTable (tail indices) subTable)
+                   table
+      )
+    Nothing -> LoopIterRecord
+      (DMap.insert (head indices) (createTupleTable (tail indices)) table)
 
 collapseIterTable :: TupleTable -> TupleTable
-collapseIterTable (LoopIterRecord iterTable) = foldl (\accum (key, newTable) -> insertIfNotRepresented key newTable accum) Empty (zip allowedValues subTables)
-            where
-                allowedValues = DMap.keys iterTable
-                subTables = map (\x -> DMap.findWithDefault Empty x iterTable) allowedValues
+collapseIterTable (LoopIterRecord iterTable) = foldl
+  (\accum (key, newTable) -> insertIfNotRepresented key newTable accum)
+  Empty
+  (zip allowedValues subTables)
+ where
+  allowedValues = DMap.keys iterTable
+  subTables = map (\x -> DMap.findWithDefault Empty x iterTable) allowedValues
 
 insertIfNotRepresented :: Int -> TupleTable -> TupleTable -> TupleTable
-insertIfNotRepresented key newItem Empty = LoopIterRecord (DMap.insert key newItem DMap.empty)
-insertIfNotRepresented key newItem (LoopIterRecord table) = if not (elem newItem representedItems) then LoopIterRecord (DMap.insert key newItem table) else LoopIterRecord table
-            where
-                representedItems = map (\x -> DMap.findWithDefault Empty x table) (DMap.keys table)
+insertIfNotRepresented key newItem Empty =
+  LoopIterRecord (DMap.insert key newItem DMap.empty)
+insertIfNotRepresented key newItem (LoopIterRecord table) =
+  if not (elem newItem representedItems)
+    then LoopIterRecord (DMap.insert key newItem table)
+    else LoopIterRecord table
+ where
+  representedItems =
+    map (\x -> DMap.findWithDefault Empty x table) (DMap.keys table)
 
 createTupleTable :: [Int] -> TupleTable
-createTupleTable [] = Empty
-createTupleTable indices = LoopIterRecord (DMap.insert (head indices) (createTupleTable (tail indices)) DMap.empty)
+createTupleTable []      = Empty
+createTupleTable indices = LoopIterRecord
+  (DMap.insert (head indices) (createTupleTable (tail indices)) DMap.empty)
 
-lookupTupleTable :: [Int] -> TupleTable -> Maybe(TupleTable)
+lookupTupleTable :: [Int] -> TupleTable -> Maybe (TupleTable)
 lookupTupleTable [index] (LoopIterRecord table) = DMap.lookup index table
-lookupTupleTable (index:indices) (LoopIterRecord table) = lookupTupleTable indices (DMap.findWithDefault Empty index table)
+lookupTupleTable (index : indices) (LoopIterRecord table) =
+  lookupTupleTable indices (DMap.findWithDefault Empty index table)
 lookupTupleTable _ _ = Nothing
 
 tupleTableNotEmpty :: TupleTable -> Bool
