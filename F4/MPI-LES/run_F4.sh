40dc0a360f6f30d1badd207631935b24fd1baf4f
james <jameslmacdonald@gmail.com>
2019-03-18 17:05:14 +0000

    parse the loop varaible usage order from exprs rather than assuming they are used in order of nesting ... as would make sense

--- F4/MPI-LES/run_F4.sh
+++ F4/MPI-LES/run_F4.sh
@@ -1,2 +1,3 @@
+stack build
 rm out.log
 F4-exe -m main.f95 -s ./src -o adam -o bondv1 -o feedbf -o les -o press -o velfg -o velnw -D WV_OPENCL,WV_NEW,WV_NEW_VELFG,NO_GLOBAL_SOR,INLINE_BOUND_CALCS,WV_TEST,TWINNED_BUFFER,NO_IO,IFBF=1,IADAM=0 >> out.log 2>&1
