86644a726fe0d5f8e3a72f424b01145109a35751
james <jameslmacdonald@gmail.com>
2019-03-22 13:24:12 +0000

    move lappy

--- F4/src/DetectIndividualPipelines.hs
+++ F4/src/DetectIndividualPipelines.hs
@@ -16,85 +16,72 @@ splitMergedMethodInPipelines MkSubRec {..} = do
   return []
 
 getTopLevelBlockStatements :: Fortran Anno -> IO [Fortran Anno]
-getTopLevelBlockStatements mergedBody
+getTopLevelBlockStatements mergedBody = do
+  mapM_
+    (\nest -> do
+      putStrLn $ miniPPF nest
+      putStrLn $ show $ getNestingLevelAndIteration nest
+      putStrLn $ "\n----------------------------------------\n"
+    )
+    loopNests
+  return []
   -- mapM_
   --   (\nest ->
   --      putStrLn $ miniPPF nest ++
   --   loopNests
- = do
-  mapM_
-    (\nest -> do
-       putStrLn $ miniPPF nest
-       putStrLn $ show $ getNestingLevelAndIteration nest
-       putStrLn $ "\n----------------------------------------\n")
-    loopNests
-  return []
-  where
-    loopNests = getLoopNests mergedBody
+  where loopNests = getLoopNests mergedBody
 
 getLoopNests :: Fortran Anno -> [Fortran Anno]
 getLoopNests = go "" []
-  where
-    go :: String -> [Fortran Anno] -> Fortran Anno -> [Fortran Anno]
-    go _ _ (OriginalSubContainer _ name body) = go name [] body
-    go name preamble (FSeq _ _ f1 f2)
-      | isLoop f1 = osc name (block (preamble ++ [f1])) : go name [] f2
-      | isLoop f2 = [osc name (block $ preamble ++ [f1, f2])]
-      | isOSC f1 = go "" [] f1 ++ go "" [] f2
-      | isOSC f2 = go "" [] f2
-      | isFSeq f2 = go name (preamble ++ [f1]) f2
-      | otherwise = error "can't find main kernel body after preamble"
-    go _ _ (NullStmt _ _) = []
-    go name preamble fortran
-      | isLoop fortran = [osc name (block $ preamble ++ [fortran])]
-      | otherwise = error "encountered fortran that is not main body"
+ where
+  go :: String -> [Fortran Anno] -> Fortran Anno -> [Fortran Anno]
+  go _ _ (OriginalSubContainer _ name body) = go name [] body
+  go name preamble (FSeq _ _ f1 f2)
+    | isLoop f1 = osc name (block (preamble ++ [f1])) : go name [] f2
+    | isLoop f2 = [osc name (block $ preamble ++ [f1, f2])]
+    | isOSC f1  = go "" [] f1 ++ go "" [] f2
+    | isOSC f2  = go "" [] f2
+    | isFSeq f2 = go name (preamble ++ [f1]) f2
+    | otherwise = error "can't find main kernel body after preamble"
+  go _ _ (NullStmt _ _) = []
+  go name preamble fortran
+    | isLoop fortran = [osc name (block $ preamble ++ [fortran])]
+    | otherwise      = error "encountered fortran that is not main body"
 
-isOSC fortran =
-  case fortran of
-    OriginalSubContainer {} -> True
-    _                       -> False
+isOSC fortran = case fortran of
+  OriginalSubContainer{} -> True
+  _                      -> False
 
-isLoop fortran =
-  case fortran of
-    For {} -> True
-    _      -> False
+isLoop fortran = case fortran of
+  For{} -> True
+  _     -> False
 
 getNestingLevelAndIteration :: Fortran Anno -> (String, Int, Int)
 getNestingLevelAndIteration (OriginalSubContainer _ name body) =
   descendLoopNest (name, 0, 1) body
-  where
-    descendLoopNest :: (String, Int, Int) -> Fortran Anno -> (String, Int, Int)
-    descendLoopNest current (FSeq _ _ f1 f2) =
-      case result1Larger of
-        Prelude.GT -> result1
-        Prelude.LT -> result2
-        _          -> result1
-      where
-        result1@(_, nl1, ic1) = descendLoopNest current f1 -- FIXME this probably needs to do something with the other FSeq branch
-        result2@(_, nl2, ic2) = descendLoopNest current f2
-        result1Larger = nl1 `compare` nl2 <> ic1 `compare` ic2
-    descendLoopNest (name, nestingLevel, totalIterations) (For _ _ _ initExpr upbExpr _ body) =
-      descendLoopNest
-        -- trace
-        --    ("name = " ++
-        --     name ++
-        --     " nestingLevel = " ++
-        --     show nestingLevel ++
-        --     " totalIterations = " ++
-        --     show totalIterations ++
-        --     " lwbInt = " ++ show lwbInt ++ " upbInt = " ++ show upbInt)
-        ((name, nestingLevel + 1, totalIterations * (upbInt - lwbInt + 1)))
-        body
-      where
-        lwbInt = getIntFromExpr initExpr
-        upbInt = getIntFromExpr upbExpr
-    descendLoopNest result _ = result
+ where
+  descendLoopNest :: (String, Int, Int) -> Fortran Anno -> (String, Int, Int)
+  descendLoopNest current (FSeq _ _ f1 f2) = case result1Larger of
+    Prelude.GT -> result1
+    Prelude.LT -> result2
+    _          -> result1
+   where
+    result1@(_, nl1, ic1) = descendLoopNest current f1
+    result2@(_, nl2, ic2) = descendLoopNest current f2
+    result1Larger         = nl1 `compare` nl2 <> ic1 `compare` ic2
+  descendLoopNest (name, nestingLevel, totalIterations) (For _ _ _ initExpr upbExpr _ body)
+    = descendLoopNest
+      (name, nestingLevel + 1, totalIterations * (upbInt - lwbInt + 1))
+      body
+   where
+    lwbInt = getIntFromExpr initExpr
+    upbInt = getIntFromExpr upbExpr
+  descendLoopNest result _ = result
 
 getIntFromExpr :: Expr Anno -> Int
 getIntFromExpr expr = head $ everything (++) (mkQ [] intConQuery) expr
 
 intConQuery :: Expr Anno -> [Int]
-intConQuery expr =
-  case expr of
-    (Con _ _ val) -> [read val :: Int]
-    _             -> [] -- error "More complicated loop expr than u thought"
+intConQuery expr = case expr of
+  (Con _ _ val) -> [read val :: Int]
+  _             -> [] -- error "More complicated loop expr than u thought"
--- F4/temp.f95
+++ F4/temp.f95
@@ -1,25 +1,57 @@
-real(4), dimension(0:1, 0:302, 0:302, 0:81) :: p
-real(4), dimension(0:301, -1:301, 0:81), intent(InOut) :: u
-real(4), dimension(0:300, 0:300, 0:80), intent(InOut) :: f
-! OpenCLStencil (
-!        2 point stencil on 4D array p: [[C=0,1,0,0],[C=0,0,0,0]]
-!    ){
-! OpenCLMap ( ["ro","dxs","dt","k","j","u"],["u"],["(k,1,80,1)","(j,1,300,1)","(i,1,300,1)"],[]) {
-pz = (-p(0, i, j, k) + p(0, i + 1, j, k))/ro/dxs(i)
-u(i, j, k) = u(i, j, k) + dt*(f(i, j, k) - pz)
-!}
-!}
-
-
-
-real, dimension(0:501,0:501) :: du
-real, dimension(0:501,0:501) :: dv
-real, dimension(0:501,0:501) :: eta
-! OpenCLStencil (
-!	3 point stencil on 2D array eta: [[1,0],[0,0],[0,1]]
-!    ){
-! OpenCLMap ( ["dt","g","dx","dy"],["du","dv"],["(j,1,500,1)","(k,1,500,1)"],[]) {
-du(j,k) = -dt*g*(eta(j,k+1)-eta(j,k))/dx
-dv(j,k) = -dt*g*(eta(j+1,k)-eta(j,k))/dy
-!}    
+! original subroutine name: press {
+    do l = 1, 50, 1
+        sor = 0.0
+        do nrd = 0, 1, 1
+            do k = 1, 80, 1
+                do j = 1, 300, 1
+                    do i = 1, 300, 1
+                        dz1 = dzs(k-1)
+                        dz2 = dzs(k)
+                        cn4s = 2./(dz1*(dz1+dz2))
+                        cn4l = 2./(dz2*(dz1+dz2))
+                        cn3s = 2./(dys(j-1)*(dys(j-1)+dys(j)))
+                        cn3l = 2./(dys(j)*(dys(j-1)+dys(j)))
+                        cn2s = 2./(dxs(i-1)*(dxs(i-1)+dxs(i)))
+                        cn2l = 2./(dxs(i)*(dxs(i-1)+dxs(i)))
+                        cn1 = 1./(2./(dxs(i-1)*dxs(i))+2./(dys(j-1)*dys(j))+2./(dz1*dz2))
+                        if (nrd==0) then
+                            reltmp = 1.0*(cn1*(cn2l*p(0,i+1,j,k)+cn2s*p(0,i-1,j,k)+cn3l*p(0,i,j+1,k)+cn3s*p(0,i,j-1,k)+cn4l*p(0,i,j,k+1)+cn4s*p(0,i,j,k-1)-rhs(i,j,k))-p(0,i,j,k))
+                            p(1,i,j,k) = p(0,i,j,k)+reltmp
+                        else
+                            reltmp = 1.0*(cn1*(cn2l*p(1,i+1,j,k)+cn2s*p(1,i-1,j,k)+cn3l*p(1,i,j+1,k)+cn3s*p(1,i,j-1,k)+cn4l*p(1,i,j,k+1)+cn4s*p(1,i,j,k-1)-rhs(i,j,k))-p(1,i,j,k))
+                            p(0,i,j,k) = p(1,i,j,k)+reltmp
+                        end if
+                    end do
+                end do
+            end do
+        end do
+    end do
+    do l = 1, 50, 1
+        do nrd = 0, 1, 1
+            do k = 0, 81, 1
+                do j = 0, 301, 1
+                    p(0,0,j,k) = p(0,1,j,k)
+                    p(0,301,j,k) = p(0,300,j,k)
+                end do
+            end do
+        end do
+    end do
+    do l = 1, 50, 1
+        do nrd = 0, 1, 1
+            do k = 0, 81, 1
+                do i = 0, 301, 1
+                    p(0,i,0,k) = p(0,i,300,k)
+                    p(0,i,301,k) = p(0,i,1,k)
+                end do
+            end do
+        end do
+    end do
+    do l = 1, 50, 1
+        do j = 0, 301, 1
+            do i = 0, 301, 1
+                p(0,i,j,0) = p(0,i,j,1)
+                p(0,i,j,81) = p(0,i,j,80)
+            end do
+        end do
+    end do
 !}
