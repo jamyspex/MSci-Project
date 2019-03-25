c4044595fae45af64b39d606c6c2e717dde917ec
james <jameslmacdonald@gmail.com>
2019-03-25 13:48:21 +0000

    move laptop

--- F4/src/F4.hs
+++ F4/src/F4.hs
@@ -10,12 +10,20 @@ import           AddSmartCaches
 import           AddSynthesisedLoopVars
 import           AddTransitStreams
 import           BuildDeviceModule
-import           CommandLineProcessor        (F4Opts (..), f4CmdParser)
+import           CommandLineProcessor           ( F4Opts(..)
+                                                , f4CmdParser
+                                                )
 import           Control.Monad.Extra
-import           Data.Generics               (everything, everywhere,
-                                              everywhereM, gmapQ, gmapT, mkM,
-                                              mkQ, mkT)
-import qualified Data.Map                    as DMap
+import           Data.Generics                  ( everything
+                                                , everywhere
+                                                , everywhereM
+                                                , gmapQ
+                                                , gmapT
+                                                , mkM
+                                                , mkQ
+                                                , mkT
+                                                )
+import qualified Data.Map                      as DMap
 import           Debug.Trace
 import           DetectDriverLoopSize
 import           DetectIndividualPipelines
@@ -23,13 +31,13 @@ import           KernelCodeGen
 import           KernelExtraction
 import           Language.Fortran
 import           Language.Fortran.Pretty
-import qualified LanguageFortranTools        as LFT
+import qualified LanguageFortranTools          as LFT
 import           LinkReductionVars
 import           MemoryAccessCodeGen
 import           MergeSubroutines
 import           MiniPP
 import           Options.Applicative
-import           Parser                      (parseProgramData)
+import           Parser                         ( parseProgramData )
 import           RemoveConstantsFromStencils
 import           SanityChecks
 import           ScalarizeKernels
@@ -55,8 +63,8 @@ compilerMain args = do
   subroutineTable <- parseProgramData args
   -- seperate out the parsed files to be offloaded to the FPGA
   let notForOffloadSubTable = DMap.filter (not . parallelise) subroutineTable
-  let forOffloadSubTable = DMap.filter parallelise subroutineTable
-  let subroutineNames = DMap.keys forOffloadSubTable
+  let forOffloadSubTable    = DMap.filter parallelise subroutineTable
+  let subroutineNames       = DMap.keys forOffloadSubTable
   putStrLn (rule '+' ++ " Subroutines not for offload " ++ rule '+')
   debug_displaySubRoutineTable notForOffloadSubTable False
   putStrLn (rule '+' ++ " Subroutines for offload " ++ rule '+')
@@ -71,22 +79,23 @@ compilerMain args = do
   let mergedOffloadName = head $ DMap.keys mergedForOffload
   putStrLn (rule '+' ++ " Stencil Constant Removal " ++ rule '+')
   removeConstantsFromStencils $ mergedForOffload DMap.! mergedOffloadName
+  error "Exit!"
   putStrLn (rule '+' ++ " Pipeline Detection " ++ rule '+')
   splitMergedMethodInPipelines $ mergedForOffload DMap.! mergedOffloadName
   putStrLn (rule '+' ++ " Map + Fold Detection " ++ rule '+')
   -- Map and fold detection from Gavin's compiler
   -- < STEP 4 : Parallelise the loops >
   -- WV: this is the equivalent of calling a statefull pass on every subroutine.
-  let (parallelisedSubroutines, parAnnotations) =
-        foldl
-          (paralleliseProgUnit_foldl
-             (ioSubs args)
-             subroutineTableWithOffloadSubsMerged)
-          (DMap.empty, [])
-          [mergedOffloadName]
+  let (parallelisedSubroutines, parAnnotations) = foldl
+        (paralleliseProgUnit_foldl (ioSubs args)
+                                   subroutineTableWithOffloadSubsMerged
+        )
+        (DMap.empty, [])
+        [mergedOffloadName]
   debug_displaySubRoutineTable parallelisedSubroutines False
-  let srtWithParallelisedSubroutines =
-        DMap.union parallelisedSubroutines subroutineTableWithOffloadSubsMerged
+  let srtWithParallelisedSubroutines = DMap.union
+        parallelisedSubroutines
+        subroutineTableWithOffloadSubsMerged
   -- mapM_ (\subRecord -> putStrLn ("\n" ++ hl ++ (fst subRecord) ++ hl ++ (miniPPProgUnit (subAst (snd subRecord))) ++ hl))
   --     (DMap.toList parallelisedSubroutines)
   putStrLn (rule '+' ++ " Stencil Detection " ++ rule '+')
@@ -94,11 +103,10 @@ compilerMain args = do
         detectStencilsInSubsToBeParallelise srtWithParallelisedSubroutines
   debug_displaySubRoutineTable srtAfterStenDetect False
   -- < STEP 5 : Try to fuse the parallelised loops as much as possible (on a per-subroutine basis) >
-  let (combinedKernelSubroutines, combAnnotations) =
-        foldl
-          (combineKernelProgUnit_foldl (loopFusionBound args))
-          (srtAfterStenDetect, [])
-          [mergedOffloadName]
+  let (combinedKernelSubroutines, combAnnotations) = foldl
+        (combineKernelProgUnit_foldl (loopFusionBound args))
+        (srtAfterStenDetect, [])
+        [mergedOffloadName]
   putStrLn (rule '+' ++ " Combined " ++ rule '+')
   let srtAfterKernelCombination =
         DMap.union combinedKernelSubroutines srtAfterStenDetect
@@ -111,9 +119,9 @@ compilerMain args = do
   debug_displaySubRoutineTable srtWithGuards False
   putStrLn (rule '+' ++ " Kernels " ++ rule '+')
   let guardedMerged = srtWithGuards DMap.! mergedOffloadName
-  kernels <- getKernels guardedMerged
+  kernels          <- getKernels guardedMerged
   driverLoopParams <- detectDriverLoopSize kernels
-  let mainSubName = mainSub args
+  let mainSubName  = mainSub args
   let mainArgTrans = argTranslations (notForOffloadSubTable DMap.! mainSubName)
   putStrLn "BEFORE"
   kernelsWithTransitStreams <- addTransitStreams kernels
@@ -132,7 +140,7 @@ compilerMain args = do
   putStrLn (rule '+' ++ " Routing Pipes " ++ rule '+')
   withPipes <- populatePipes withSharedDataUpdated
   putStrLn (rule '+' ++ " Scalarizing Kernels " ++ rule '+')
-  scalarisedKernels <- scalarizeKernels withPipes
+  scalarisedKernels                   <- scalarizeKernels withPipes
   (fileName, deviceCode, callingData) <- buildDeviceModule scalarisedKernels
   mapM_ print callingData
   writeToFile args (fileName ++ ".f95") (miniPPProgUnit deviceCode)
@@ -143,8 +151,7 @@ writeToFile F4Opts {..} fileName contents = do
   createDirectoryIfMissing True outputDir
   whenM (doesFileExist filePath) (removeFile filePath)
   writeFile filePath contents
-  where
-    filePath = joinPath [outputDir, fileName]
+  where filePath = joinPath [outputDir, fileName]
 
 validateInputFiles :: Program LFT.Anno -> IO ()
 validateInputFiles fileAst = do
@@ -152,7 +159,8 @@ validateInputFiles fileAst = do
   mapM_ printErrorOrContinue results
 
 banner =
-  rule '=' ++
-  "F4: Finite-element Fortran for FPGAs\n" ++
-  "This compiler allows Fortran finite element codes to be compiled\n" ++
-  "for execution on FPGA devices via OpenCL" ++ rule '='
+  rule '='
+    ++ "F4: Finite-element Fortran for FPGAs\n"
+    ++ "This compiler allows Fortran finite element codes to be compiled\n"
+    ++ "for execution on FPGA devices via OpenCL"
+    ++ rule '='
--- F4/src/RemoveConstantsFromStencils.hs
+++ F4/src/RemoveConstantsFromStencils.hs
@@ -40,9 +40,8 @@ processLoopNest :: (Fortran Anno, [ArrayAccess]) -> IO (Fortran Anno)
 processLoopNest (loopNest, arrayAccesses) = do
   putStrLn $ miniPPF loopNest
   putStrLn $ show arrayAccesses
-  when
-    (isJust highestDimArrayAccessedWithConstant)
-    (putStrLn $ (show . fromJust) highestDimArrayAccessedWithConstant)
+  when (isJust highestDimArrayAccessedWithConstant)
+       (putStrLn $ (show . fromJust) highestDimArrayAccessedWithConstant)
   putStrLn $ "Nesting direction = " ++ show nestingDirection
   putStrLn "Updated = "
   putStrLn $ miniPPF withSyntheticLoopsInserted
@@ -50,40 +49,51 @@ processLoopNest (loopNest, arrayAccesses) = do
   putStrLn $ concatMap (\t -> "\t" ++ show t ++ "\n") loopVarPosTuples
   putStrLn "-------------------------------------------"
   return loopNest
-  where
-    loopVarPosTuples =
-      getLoopVarConstantPosTuples accessesValidated withSyntheticLoopsInserted
-    withSyntheticLoopsInserted =
-      insertSyntheticLoops
-        highestDimArrayAccessedWithConstant
-        (fromMaybe [] constantPositions)
-        nestingDirection
-        loopNest
-    constantPositions =
-      fmap getConstantPositions highestDimArrayAccessedWithConstant
-    highestDimArrayAccessedWithConstant =
-      getHighestDimensionArrayAccess accessesValidated
-    accessesValidated = allConstantsUsedInSamePosition arrayAccesses
-    loopVars = getLoopVariableByNestOrder loopNest
-    nestingDirection = detectNestingDirection loopVars arrayAccesses
+ where
+  loopVarPosTuples = getLoopVarConstantPosTuples accessesValidated
+                                                 nestingDirection
+                                                 withSyntheticLoopsInserted
+  withSyntheticLoopsInserted = insertSyntheticLoops
+    highestDimArrayAccessedWithConstant
+    (fromMaybe [] constantPositions)
+    nestingDirection
+    loopNest
+  constantPositions =
+    fmap getConstantPositions highestDimArrayAccessedWithConstant
+  highestDimArrayAccessedWithConstant =
+    getHighestDimensionArrayAccess accessesValidated
+  accessesValidated = allConstantsUsedInSamePosition arrayAccesses
+  loopVars          = getLoopVariableByNestOrder loopNest
+  nestingDirection  = detectNestingDirection loopVars arrayAccesses
 
-getLoopVarConstantPosTuples ::
-     [ArrayAccess] -> Fortran Anno -> [(Int, String, Index)]
-getLoopVarConstantPosTuples arrayAccesses loopNest = []
-  where
-    loopVarsOrderedByNesting = getLoopVariableByNestOrder loopNest
-    mostIndicesCount =
-      (length . indices . getHighestDimensionArrayAccessNoMaybe) arrayAccesses
-    buildTuple :: [String] -> ArrayAccess -> [(Int, String, Index)]
-    buildTuple loopVars AA {..} =
-      imap
+getLoopVarConstantPosTuples
+  :: [ArrayAccess]
+  -> NestingDirection
+  -> Fortran Anno
+  -> [(Int, String, String, Index)]
+getLoopVarConstantPosTuples arrayAccesses nestDirection loopNest = concatMap
+  (buildTuple loopVarsOrderedByNesting)
+  arrayAccesses
+ where
+  loopVarsOrderedByNesting = case nestDirection of
+    Reverse -> reverse $ getLoopVariableByNestOrder loopNest
+    Normal  -> getLoopVariableByNestOrder loopNest
+    _       -> error "Unsupported nesting direction"
+  mostIndicesCount = trace
+    ("loopVarsOrderedByNesting = " ++ show loopVarsOrderedByNesting)
+    (length . indices . getHighestDimensionArrayAccessNoMaybe)
+    arrayAccesses
+  buildTuple :: [String] -> ArrayAccess -> [(Int, String, String, Index)]
+  buildTuple loopVars AA {..} =
+    imap
         (\pos (loopVar, index) ->
-           let loopVarIndex = pos + (mostIndicesCount - length indices)
-            in (pos, loopVars !! loopVarIndex, index)) $
-      zip loopVars indices
+          let loopVarIndex = pos + (mostIndicesCount - length indices)
+          in  (pos, loopVars !! loopVarIndex, arrayName, index)
+        )
+      $ zip loopVars indices
 
-insertSyntheticLoops ::
-     Maybe ArrayAccess
+insertSyntheticLoops
+  :: Maybe ArrayAccess
   -> [Bool]
   -> NestingDirection
   -> Fortran Anno
@@ -94,131 +104,128 @@ insertSyntheticLoops (Just AA {..}) constantPositions Normal loopNest =
 insertSyntheticLoops (Just AA {..}) constantPositions Reverse loopNest =
   addLoops (reverse declaredDimensions) (reverse constantPositions) loopNest
 
-addLoops dims insertLoop wholeLoopNest =
-  addLoops' maxNestLevel 0 dims insertLoop wholeLoopNest
-  where
-    maxNestLevel = length dims - 1
+addLoops dims insertLoop wholeLoopNest = addLoops' maxNestLevel
+                                                   0
+                                                   dims
+                                                   insertLoop
+                                                   wholeLoopNest
+  where maxNestLevel = length dims - 1
 
-addLoops' ::
-     Int -> Int -> [(Int, Int)] -> [Bool] -> Fortran Anno -> Fortran Anno
+addLoops'
+  :: Int -> Int -> [(Int, Int)] -> [Bool] -> Fortran Anno -> Fortran Anno
 addLoops' maxLevel level dims insertLoop loopNest
-  | level == maxLevel && loopBodyStatementsOnly loopNest && insertLoop !! level =
-    for ("synthIdx" ++ show level) lwb (con upb) loopNest
-  | level == maxLevel && loopBodyStatementsOnly loopNest = loopNest
-  | insertLoop !! level && isJust originalFor =
-    for
-      ("synthIdx" ++ show level)
-      lwb
-      (con upb)
-      (origFor
-         (addLoops'
-            maxLevel
-            (level + 1)
-            dims
-            insertLoop
-            (stripLoopNest loopNest)))
-  | insertLoop !! level =
-    for
-      ("synthIdx" ++ show level)
-      lwb
-      (con upb)
+  | level == maxLevel && loopBodyStatementsOnly loopNest && insertLoop !! level
+  = for ("synthIdx" ++ show level) lwb (con upb) loopNest
+  | level == maxLevel && loopBodyStatementsOnly loopNest
+  = loopNest
+  | insertLoop !! level && isJust originalFor
+  = for
+    ("synthIdx" ++ show level)
+    lwb
+    (con upb)
+    (origFor
       (addLoops' maxLevel (level + 1) dims insertLoop (stripLoopNest loopNest))
-  | isJust originalFor =
-    origFor $
-    addLoops' maxLevel (level + 1) dims insertLoop (stripLoopNest loopNest)
-  | otherwise =
-    addLoops' maxLevel (level + 1) dims insertLoop (stripLoopNest loopNest)
-  where
-    (lwb, upb) = dims !! level
-    origFor = fromJust originalFor
-    originalFor =
-      case getNearestFor loopNest of
-        For anno srcSpan loopVarName expr1 expr2 expr3 fortran ->
-          Just (For anno srcSpan loopVarName expr1 expr2 expr3)
-        _ -> Nothing
+    )
+  | insertLoop !! level
+  = for
+    ("synthIdx" ++ show level)
+    lwb
+    (con upb)
+    (addLoops' maxLevel (level + 1) dims insertLoop (stripLoopNest loopNest))
+  | isJust originalFor
+  = origFor
+    $ addLoops' maxLevel (level + 1) dims insertLoop (stripLoopNest loopNest)
+  | otherwise
+  = addLoops' maxLevel (level + 1) dims insertLoop (stripLoopNest loopNest)
+ where
+  (lwb, upb)  = dims !! level
+  origFor     = fromJust originalFor
+  originalFor = case getNearestFor loopNest of
+    For anno srcSpan loopVarName expr1 expr2 expr3 fortran ->
+      Just (For anno srcSpan loopVarName expr1 expr2 expr3)
+    _ -> Nothing
 
 getNearestFor :: Fortran Anno -> Fortran Anno
 getNearestFor (OriginalSubContainer _ _ body) = getNearestFor body
-getNearestFor (FSeq _ _ f1 NullStmt {})       = getNearestFor f1
-getNearestFor for@For {}                      = for
+getNearestFor (FSeq _ _ f1 NullStmt{}       ) = getNearestFor f1
+getNearestFor for@For{}                       = for
 getNearestFor body                            = body
 
 stripLoopNest :: Fortran Anno -> Fortran Anno
 stripLoopNest (OriginalSubContainer _ _ body) = stripLoopNest body
-stripLoopNest (FSeq _ _ f1 NullStmt {}) = stripLoopNest f1
-stripLoopNest (For _ _ _ _ _ _ body) = body
+stripLoopNest (FSeq _ _ f1 NullStmt{}       ) = stripLoopNest f1
+stripLoopNest (For _ _ _ _ _ _ body         ) = body
 stripLoopNest body
   | loopBodyStatementsOnly body = body
   | otherwise = error ("Can't strip loop nest from: \n" ++ miniPPF body)
 
 getHighestDimensionArrayAccessNoMaybe :: [ArrayAccess] -> ArrayAccess
-getHighestDimensionArrayAccessNoMaybe =
-  maximumBy
-    (\a1 a2 ->
-       length (declaredDimensions a1) `compare` length (declaredDimensions a2))
+getHighestDimensionArrayAccessNoMaybe = maximumBy
+  (\a1 a2 ->
+    length (declaredDimensions a1) `compare` length (declaredDimensions a2)
+  )
 
 getHighestDimensionArrayAccess :: [ArrayAccess] -> Maybe ArrayAccess
-getHighestDimensionArrayAccess allArrayAccesses =
-  if null onlyWithConstants
-    then Nothing
-    else Just mostConstants
-  where
-    mostConstants =
-      maximumBy
-        (\a1 a2 ->
-           length (declaredDimensions a1) `compare`
-           length (declaredDimensions a2))
-        onlyWithConstants
-    onlyWithConstants =
-      filter
-        (\AA {..} ->
-           any
-             (\case
-                LoopVar _ -> False
-                Const _ -> True)
-             indices)
-        allArrayAccesses
+getHighestDimensionArrayAccess allArrayAccesses = if null onlyWithConstants
+  then Nothing
+  else Just mostConstants
+ where
+  mostConstants = maximumBy
+    (\a1 a2 ->
+      length (declaredDimensions a1) `compare` length (declaredDimensions a2)
+    )
+    onlyWithConstants
+  onlyWithConstants = filter
+    (\AA {..} -> any
+      (\case
+        LoopVar _ -> False
+        Const   _ -> True
+      )
+      indices
+    )
+    allArrayAccesses
 
 allConstantsUsedInSamePosition :: [ArrayAccess] -> [ArrayAccess]
 allConstantsUsedInSamePosition allArrayAccesses =
   if all (\grp -> all (== head grp) grp) constantPositions
     then allArrayAccesses
-    else error
-           "constants are not used in the same positions across stencils of same size"
-  where
-    constantPositions = map (map getConstantPositions) grpdByLength
-    grpdByLength =
-      groupBy
-        (\a1 a2 ->
-           (length . declaredDimensions) a1 == (length . declaredDimensions) a2)
-        allArrayAccesses
+    else
+      error
+        "constants are not used in the same positions across stencils of same size"
+ where
+  constantPositions = map (map getConstantPositions) grpdByLength
+  grpdByLength      = groupBy
+    (\a1 a2 ->
+      (length . declaredDimensions) a1 == (length . declaredDimensions) a2
+    )
+    allArrayAccesses
 
 getConstantPositions :: ArrayAccess -> [Bool]
-getConstantPositions AA {..} =
-  map
-    (\case
-       LoopVar _ -> False
-       Const _ -> True)
-    indices
+getConstantPositions AA {..} = map
+  (\case
+    LoopVar _ -> False
+    Const   _ -> True
+  )
+  indices
 
 getLoopVariableByNestOrder :: Fortran Anno -> [String]
 getLoopVariableByNestOrder (OriginalSubContainer _ _ body) =
   getLoopVariableByNestOrder body
-getLoopVariableByNestOrder (FSeq _ _ f1 NullStmt {}) =
+getLoopVariableByNestOrder (FSeq _ _ f1 NullStmt{}) =
   getLoopVariableByNestOrder f1
 getLoopVariableByNestOrder (For _ _ (VarName _ loopVarName) _ _ _ body) =
   loopVarName : getLoopVariableByNestOrder body
 getLoopVariableByNestOrder _ = []
 
-getLoopNestsAndStencilAccesses ::
-     ProgUnit Anno -> IO [(Fortran Anno, [ArrayAccess])]
+getLoopNestsAndStencilAccesses
+  :: ProgUnit Anno -> IO [(Fortran Anno, [ArrayAccess])]
 getLoopNestsAndStencilAccesses mergedBody = do
   return loopNestsToArrayAccesses
-  where
-    loopNests = getInnerLoopNests $ getSubBody mergedBody
-    allArrays = map (arrayFromDeclWithRanges True) $ getDecls mergedBody
-    loopNestsToArrayAccesses =
-      map (\ln -> (ln, parseArrayAccesses allArrays ln)) loopNests
+ where
+  loopNests = getInnerLoopNests $ getSubBody mergedBody
+  allArrays = map (arrayFromDeclWithRanges True) $ getDecls mergedBody
+  loopNestsToArrayAccesses =
+    map (\ln -> (ln, parseArrayAccesses allArrays ln)) loopNests
 
 data NestingDirection
   = Normal
@@ -228,107 +235,99 @@ data NestingDirection
   deriving (Show, Eq)
 
 detectNestingDirection :: [String] -> [ArrayAccess] -> NestingDirection
-detectNestingDirection loopVarsInNestOrder arrayAccesses =
-  if valid
-    then firstNotEither
-    else error "Index usage ordering not consistent"
-  where
-    allNestingDirections = map (checkOne loopVarsInNestOrder) arrayAccesses
-    firstNotEither =
-      head $
-      filter
-        (\case
-           Either -> False
-           _ -> True)
-        allNestingDirections
-    valid = all (\v -> v == firstNotEither || v == Either) allNestingDirections
+detectNestingDirection loopVarsInNestOrder arrayAccesses = if valid
+  then firstNotEither
+  else error "Index usage ordering not consistent"
+ where
+  allNestingDirections = map (checkOne loopVarsInNestOrder) arrayAccesses
+  firstNotEither       = head $ filter
+    (\case
+      Either -> False
+      _      -> True
+    )
+    allNestingDirections
+  valid = all (\v -> v == firstNotEither || v == Either) allNestingDirections
 
 checkOne :: [String] -> ArrayAccess -> NestingDirection
-checkOne loopVars arrayAccess =
-  case (forward, backward) of
-    (True, True)   -> Either
-    (True, _)      -> Normal
-    (_, True)      -> Reverse
-    (False, False) -> error "Can not detect loop nesting direction"
-  where
-    forward = go 0 loopVars accessLoopVarsOnly
-    backward = go 0 (reverse loopVars) accessLoopVarsOnly
-    accessLoopVarsOnly =
-      (concatMap
-         (\case
+checkOne loopVars arrayAccess = case (forward, backward) of
+  (True , True ) -> Either
+  (True , _    ) -> Normal
+  (_    , True ) -> Reverse
+  (False, False) -> error "Can not detect loop nesting direction"
+ where
+  forward  = go 0 loopVars accessLoopVarsOnly
+  backward = go 0 (reverse loopVars) accessLoopVarsOnly
+  accessLoopVarsOnly =
+    ( concatMap
+          (\case
             LoopVar name -> [name]
-            Const _ -> []) .
-       indices)
-        arrayAccess
-    go :: Int -> [String] -> [String] -> Bool
-    go misMatchCount (lv:lvs) (ai:ais)
-      | misMatchCount == 2 = False
-      | lv == ai = go misMatchCount lvs ais
-      | lv /= ai = go (misMatchCount + 1) lvs ais
-      | otherwise = go misMatchCount lvs ais
-    go misMatchCount _ _
-      | misMatchCount == 2 = False
-      | otherwise = True
+            Const   _    -> []
+          )
+      . indices
+      )
+      arrayAccess
+  go :: Int -> [String] -> [String] -> Bool
+  go misMatchCount (lv : lvs) (ai : ais)
+    | misMatchCount == 2 = False
+    | lv == ai           = go misMatchCount lvs ais
+    | lv /= ai           = go (misMatchCount + 1) lvs ais
+    | otherwise          = go misMatchCount lvs ais
+  go misMatchCount _ _ | misMatchCount == 2 = False
+                       | otherwise          = True
 
 parseArrayAccesses :: [Array] -> Fortran Anno -> [ArrayAccess]
 parseArrayAccesses allArrays loopNest = uniqueArrayAccesses
-  where
-    uniqueArrayAccesses = sortUniq allParsedArrayAccesses
-    allParsedArrayAccesses = map buildArrayAccess allArrayAccessExprs
-    allArrayAccessExprs =
-      getAllArrayAccessesWithMatchingArray allArrays loopNest
-    buildArrayAccess :: (Expr Anno, Array) -> ArrayAccess
-    buildArrayAccess (accessExpr, Array {..}) =
-      AA
-        { arrayName = name
-        , indices = map buildIndex indexExprs
-        , declaredDimensions = dimensionRanges
-        }
-      where
-        (Var _ _ [(VarName _ name, indexExprs)]) = accessExpr
-        accessIndices = map
-        buildIndex :: Expr Anno -> Index
-        buildIndex expr =
-          case expr of
-            Con _ _ val -> Const (read val :: Int)
-            Bin _ _ _ lhs _ -> LoopVar $ (getNameFromVarName . getVarName) lhs
-            Var _ _ [(VarName _ name, _)] -> LoopVar name
-            expr -> error ("missing pattern for \n" ++ miniPP expr)
+ where
+  uniqueArrayAccesses = sortUniq allParsedArrayAccesses
+  allParsedArrayAccesses = map buildArrayAccess allArrayAccessExprs
+  allArrayAccessExprs = getAllArrayAccessesWithMatchingArray allArrays loopNest
+  buildArrayAccess :: (Expr Anno, Array) -> ArrayAccess
+  buildArrayAccess (accessExpr, Array {..}) = AA
+    { arrayName          = name
+    , indices            = map buildIndex indexExprs
+    , declaredDimensions = dimensionRanges
+    }
+   where
+    (Var _ _ [(VarName _ name, indexExprs)]) = accessExpr
+    accessIndices                            = map
+    buildIndex :: Expr Anno -> Index
+    buildIndex expr = case expr of
+      Con _ _ val -> Const (read val :: Int)
+      Bin _ _ _ lhs _ -> LoopVar $ (getNameFromVarName . getVarName) lhs
+      Var _ _ [(VarName _ name, _)] -> LoopVar name
+      expr -> error ("missing pattern for \n" ++ miniPP expr)
 
 loopBodyStatementsOnly :: Fortran Anno -> Bool
-loopBodyStatementsOnly fortran =
-  case fortran of
-    For {} -> False
-    FSeq _ _ f1 f2 -> loopBodyStatementsOnly f1 && loopBodyStatementsOnly f2
-    OriginalSubContainer _ _ body -> loopBodyStatementsOnly body
-    _ -> True
+loopBodyStatementsOnly fortran = case fortran of
+  For{}                         -> False
+  FSeq _ _ f1 f2 -> loopBodyStatementsOnly f1 && loopBodyStatementsOnly f2
+  OriginalSubContainer _ _ body -> loopBodyStatementsOnly body
+  _                             -> True
 
 loopBodyOnlyContainsLoop :: Fortran Anno -> Bool
-loopBodyOnlyContainsLoop fortran =
-  case fortran of
-    For {}                      -> True
-    FSeq _ _ For {} NullStmt {} -> True
-    _                           -> False
+loopBodyOnlyContainsLoop fortran = case fortran of
+  For{}                     -> True
+  FSeq _ _ For{} NullStmt{} -> True
+  _                         -> False
 
 getInnerLoopNests :: Fortran Anno -> [Fortran Anno]
 getInnerLoopNests = go "" Nothing
-  where
-    go :: String -> Maybe (Fortran Anno) -> Fortran Anno -> [Fortran Anno]
-    go name topLevel (FSeq _ _ f1 f2) =
-      go name topLevel f1 ++ go name topLevel f2
-    go _ _ (OriginalSubContainer _ name body) = go name Nothing body
-    go name Nothing topLevel@(For _ _ _ _ _ _ body)
-      | loopBodyStatementsOnly body = [osc name topLevel]
-      | loopBodyOnlyContainsLoop body = go name (Just topLevel) body
-      | otherwise = concatMap (go name Nothing) $ allFors body
-    go name (Just topLevel) (For _ _ _ _ _ _ body)
-      | loopBodyStatementsOnly body = [osc name topLevel]
-      | loopBodyOnlyContainsLoop body = go name (Just topLevel) body
-      | otherwise = concatMap (go name Nothing) $ allFors body
-    go _ _ _ = []
+ where
+  go :: String -> Maybe (Fortran Anno) -> Fortran Anno -> [Fortran Anno]
+  go name topLevel (FSeq _ _ f1 f2) =
+    go name topLevel f1 ++ go name topLevel f2
+  go _ _ (OriginalSubContainer _ name body) = go name Nothing body
+  go name Nothing topLevel@(For _ _ _ _ _ _ body)
+    | loopBodyStatementsOnly body   = [osc name topLevel]
+    | loopBodyOnlyContainsLoop body = go name (Just topLevel) body
+    | otherwise                     = concatMap (go name Nothing) $ allFors body
+  go name (Just topLevel) (For _ _ _ _ _ _ body)
+    | loopBodyStatementsOnly body   = [osc name topLevel]
+    | loopBodyOnlyContainsLoop body = go name (Just topLevel) body
+    | otherwise                     = concatMap (go name Nothing) $ allFors body
+  go _ _ _ = []
 
-allFors body =
-  case body of
-    FSeq _ _ f1 f2 -> allFors f1 ++ allFors f2
-    for@For {}     -> [for]
-    _              -> []
+allFors body = case body of
+  FSeq _ _ f1 f2 -> allFors f1 ++ allFors f2
+  for@For{}      -> [for]
+  _              -> []
