eb85f2c9bc246edd81c0d716f1c9de5476860e93
james <jameslmacdonald@gmail.com>
2019-04-06 16:29:25 +0100

    use DSL function infix

--- F4/src/FortranDSL.hs
+++ F4/src/FortranDSL.hs
@@ -3,47 +3,29 @@ module FortranDSL where
 import           Language.Fortran
 import           LanguageFortranTools
 
--- import           Utils
---      for = for' lessThanEq
-for
-     -- (Expr Anno -> Expr Anno -> Expr Anno)
- -- ->
- ::
-     String -> Int -> Expr Anno -> Fortran Anno -> Fortran Anno
--- comparision
+for :: String -> Int -> Expr Anno -> Fortran Anno -> Fortran Anno
 for loopVar initial lhs =
-  For
-    nullAnno
-    nullSrcSpan
-    (varName loopVar)
-    (con initial)
-    lhs -- (comparision (var loopVar) lhs)
-    (con 1)
+  For nullAnno nullSrcSpan (varName loopVar) (con initial) lhs (con 1)
 
 -- forLT = for' lessThan
 nullUseBlock = UseBlock (UseNil nullAnno) NoSrcLoc
 
 buildAstSeq :: (a -> a -> a) -> a -> [a] -> a
-buildAstSeq _ nullNode [] = nullNode
-buildAstSeq _ _ [statement] = statement
-buildAstSeq constructor nullNode (statement:statements) =
+buildAstSeq _ nullNode []          = nullNode
+buildAstSeq _ _        [statement] = statement
+buildAstSeq constructor nullNode (statement : statements) =
   constructor statement (buildAstSeq constructor nullNode statements)
 
 -- combines multiple conditions produced by buildLoopGuard with .and.
 combineWithAnd :: [Expr Anno] -> Expr Anno
-combineWithAnd =
-  buildAstSeq
-    (Bin nullAnno nullSrcSpan (And nullAnno))
-    (NullExpr nullAnno nullSrcSpan)
+combineWithAnd = buildAstSeq (Bin nullAnno nullSrcSpan (And nullAnno))
+                             (NullExpr nullAnno nullSrcSpan)
 
 argNode :: [String] -> Arg Anno
-argNode names =
-  Arg
-    nullAnno
-    (buildAstSeq (ASeq nullAnno) (NullArg nullAnno) argNames)
-    nullSrcSpan
-  where
-    argNames = map (ArgName nullAnno) names
+argNode names = Arg nullAnno
+                    (buildAstSeq (ASeq nullAnno) (NullArg nullAnno) argNames)
+                    nullSrcSpan
+  where argNames = map (ArgName nullAnno) names
 
 eq = Bin nullAnno nullSrcSpan (RelEQ nullAnno)
 
@@ -69,61 +51,51 @@ argName = ArgName nullAnno
 
 block = buildAstSeq (FSeq nullAnno nullSrcSpan) (NullStmt nullAnno nullSrcSpan)
 
-fortranModule name =
-  Module
-    nullAnno
-    nullSrcSpan
-    (SubName nullAnno name)
-    (UseNil nullAnno)
-    (ImplicitNull nullAnno)
+fortranModule name = Module nullAnno
+                            nullSrcSpan
+                            (SubName nullAnno name)
+                            (UseNil nullAnno)
+                            (ImplicitNull nullAnno)
 
-sub name decls body args =
-  Sub
-    nullAnno
-    nullSrcSpan
-    Nothing
-    (SubName nullAnno name)
-    (if null args
-       then Arg nullAnno (NullArg nullAnno) nullSrcSpan
-       else Arg
-              nullAnno
-              (buildAstSeq (ASeq nullAnno) (NullArg nullAnno) args)
-              nullSrcSpan)
-    block
-  where
-    block =
-      Block nullAnno nullUseBlock (ImplicitNull nullAnno) nullSrcSpan decls body
+sub name decls body args = Sub
+  nullAnno
+  nullSrcSpan
+  Nothing
+  (SubName nullAnno name)
+  (if null args
+    then Arg nullAnno (NullArg nullAnno) nullSrcSpan
+    else Arg nullAnno
+             (buildAstSeq (ASeq nullAnno) (NullArg nullAnno) args)
+             nullSrcSpan
+  )
+  block
+ where
+  block =
+    Block nullAnno nullUseBlock (ImplicitNull nullAnno) nullSrcSpan decls body
 
-argList args =
-  ArgList nullAnno $
-  buildAstSeq
-    (ESeq nullAnno nullSrcSpan)
-    (NullExpr nullAnno nullSrcSpan)
-    argsAsVars
-  where
-    argsAsVars = map var args
+argList args = ArgList nullAnno $ buildAstSeq (ESeq nullAnno nullSrcSpan)
+                                              (NullExpr nullAnno nullSrcSpan)
+                                              argsAsVars
+  where argsAsVars = map var args
 
 ifLTCon variableName compareTo = ifLT variableName (con compareTo)
 
-ifLT variableName compareTo body =
-  If
-    nullAnno
-    nullSrcSpan
-    (lessThan (var variableName) compareTo)
-    body
-    []
-    Nothing
+ifLT variableName compareTo body = If nullAnno
+                                      nullSrcSpan
+                                      (lessThan (var variableName) compareTo)
+                                      body
+                                      []
+                                      Nothing
 
 ifGECon variableName compareTo = ifGE variableName (con compareTo)
 
-ifGE variableName compareTo body =
-  If
-    nullAnno
-    nullSrcSpan
-    (greaterThanEq (var variableName) compareTo)
-    body
-    []
-    Nothing
+ifGE variableName compareTo body = If
+  nullAnno
+  nullSrcSpan
+  (greaterThanEq (var variableName) compareTo)
+  body
+  []
+  Nothing
 
 call name args = Call nullAnno nullSrcSpan (var name) (argList args)
 
@@ -133,35 +105,35 @@ pragma text = TextStmt nullAnno nullSrcSpan ("!$PRAGMA " ++ text)
 
 declNode = buildAstSeq (DSeq nullAnno) (NullDecl nullAnno nullSrcSpan)
 
-bufferDecl name bounds valueType =
-  Decl nullAnno nullSrcSpan [(var name, nullExpr, Nothing)] bufType
-  where
-    bufType =
-      BaseType
-        nullAnno
-        valueType
-        [Dimension nullAnno (map (\(lwb, upb) -> (con lwb, con upb)) bounds)]
-        nullExpr
-        nullExpr
-
-typedDecl name valueType =
-  Decl nullAnno nullSrcSpan [(var name, nullExpr, Nothing)] declType
-  where
-    declType = BaseType nullAnno valueType [] nullExpr nullExpr
-
-intDecl name =
-  Decl
+bufferDecl name bounds valueType = Decl nullAnno
+                                        nullSrcSpan
+                                        [(var name, nullExpr, Nothing)]
+                                        bufType
+ where
+  bufType = BaseType
     nullAnno
-    nullSrcSpan
-    [(var name, nullExpr, Nothing)]
-    (BaseType nullAnno (Integer nullAnno) [] nullExpr nullExpr)
+    valueType
+    [Dimension nullAnno (map (\(lwb, upb) -> (con lwb, con upb)) bounds)]
+    nullExpr
+    nullExpr
 
-intParam name val =
-  Decl
-    nullAnno
-    nullSrcSpan
-    [(var name, con val, Nothing)]
-    (BaseType nullAnno (Integer nullAnno) [Parameter nullAnno] nullExpr nullExpr)
+typedDecl name valueType = Decl nullAnno
+                                nullSrcSpan
+                                [(var name, nullExpr, Nothing)]
+                                declType
+  where declType = BaseType nullAnno valueType [] nullExpr nullExpr
+
+intDecl name = Decl
+  nullAnno
+  nullSrcSpan
+  [(var name, nullExpr, Nothing)]
+  (BaseType nullAnno (Integer nullAnno) [] nullExpr nullExpr)
+
+intParam name val = Decl
+  nullAnno
+  nullSrcSpan
+  [(var name, con val, Nothing)]
+  (BaseType nullAnno (Integer nullAnno) [Parameter nullAnno] nullExpr nullExpr)
 
 varC name = var [name]
 
